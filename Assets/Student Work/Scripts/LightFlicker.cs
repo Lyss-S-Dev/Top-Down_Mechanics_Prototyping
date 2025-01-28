00cfef4fb2f6f28e4a85d810baed6d9a69ffc1b0
Lyss-S-Dev <s015543h@student.staffs.ac.uk>
2025-01-28 10:33:41 +0000

    Lighting Stuff
    Added lights
    wheeeeeeeeeeee
    Make Loot
    Make Death Sequence
    Make End of Level

---
+++ Assets/Student Work/Scripts/LightFlicker.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light2D objectLight;
+    [SerializeField] private float minimumIntensity = 0.5f;
+    [SerializeField] private float maximumIntensity = 2.0f;
+
+    [SerializeField] private float minFlickerTime = 0.1f;
+    [SerializeField] private float maxFlickerTime = 0.3f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        objectLight = GetComponentInChildren<Light2D>();
+        StartCoroutine(Flicker());
+    }
+
+    private IEnumerator Flicker()
+    {
+        while (true)
+        {
+            objectLight.intensity = Random.Range(minimumIntensity, maximumIntensity);
+            yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+        }
+    }
+    
+}
